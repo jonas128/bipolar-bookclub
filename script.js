b83080114007750182fed546dcb60fc4518acc49
jonas128 <jonasm@outlook.com>
2022-12-08 21:46:58 +0100

    fixed image and color change

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ const books = [
   { name: '4', image: 'images/4.jpeg' },
   { name: '5', image: 'images/5.jpeg' },
   { name: '6', image: 'images/6.jpeg' },
+  { name: '7', image: 'images/7.jpeg' },
 ];
 
 let img = document.querySelector('img');
 
 let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
-let btn3 = document.querySelector('#btn3');
 
-const colors = ['red', 'green', 'blue', 'tomato'];
-
+const colors = ['pink', 'red', 'green', 'blue', 'tomato', 'white'];
+document.body.style.backgroundColor = 'pink';
 const button = document.querySelector('button');
 
-let currentColorIndex = 0;
+let colorIndex = 1;
+let bookIndex = 0;
 
-btn3.addEventListener('click', () => {
-  document.body.style.color = colors[currentColorIndex];
+btn1.addEventListener('click', () => {
+  document.body.style.backgroundColor = colors[colorIndex];
+  colorIndex++;
 
-  currentColorIndex++;
+  bookIndex++;
+  img.src = books[bookIndex].image;
 
-  if (currentColorIndex > colors.length) {
-    currentColorIndex = 0;
+  if (bookIndex >= colors.length) {
+    colorIndex = 0;
+    bookIndex = -1;
+    document.body.style.backgroundColor = 'black';
   }
 });
 
-for (let i = 1; i < 6; i++) {
-  btn1.addEventListener('click', () => {
-    img.src = books[3].image;
-  });
-  btn2.addEventListener('click', () => {
-    img.src = 'images/6.jpeg';
-  });
-}
+btn2.addEventListener('click', () => {
+  img.src = 'images/book.ico';
+});
