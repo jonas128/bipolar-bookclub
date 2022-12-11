df7b694fb722797cfbf65fabeeb5cfeb21079a0d
jonas128 <jonasm@outlook.com>
2022-12-11 03:41:00 +0100

    fixed output and restart

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const books = [
-  { name: '1', image: 'images/1.jpeg' },
+  { name: 'This book is 29% bipolar', image: 'images/1.jpeg' },
   { name: '2', image: 'images/2.jpeg' },
   { name: '3', image: 'images/3.jpeg' },
   { name: '4', image: 'images/4.jpeg' },
-  { name: '5', image: 'images/5.jpeg' },
+  { name: 'This is not a book', image: 'images/5.jpeg' },
   { name: '6', image: 'images/6.jpeg' },
-  { name: '7', image: 'images/7.jpeg' },
+  { name: 'Non-bipolar data book', image: 'images/7.jpeg' },
 ];
 
 let img = document.querySelector('img');
@@ -21,8 +21,16 @@ let colorIndex = 1;
 let bookIndex = 0;
 
 btn1.addEventListener('click', () => {
+  let header = document.querySelector('h1');
+  header.innerText = 'Are you bipolar?';
+
   document.body.style.backgroundColor = colors[colorIndex];
   colorIndex++;
+
+  if (bookIndex == 6) {
+    bookIndex = -1;
+    // colorIndex = 0;
+  }
 
   bookIndex++;
   img.src = books[bookIndex].image;
@@ -30,10 +38,39 @@ btn1.addEventListener('click', () => {
   if (bookIndex >= colors.length) {
     colorIndex = 0;
     bookIndex = -1;
-    document.body.style.backgroundColor = 'black';
+    document.body.style.backgroundColor = 'grey';
   }
 });
 
 btn2.addEventListener('click', () => {
+  // for (let i = 0; i < 7; i++) {
+  //   setTimeout(loop, 500);
+  //   img.src = books[i].image;
+  // }
+  // img.src = books[1].image;
+  // setTimeout(loop, 500);
+  // img.src = books[2].image;
+
+  if (bookIndex == -1) {
+    bookIndex = 6;
+    // colorIndex = 0;
+  }
+
+  setTimeout(loop, 1000);
   img.src = 'images/book.ico';
+
+  let header = document.querySelector('h1');
+  header.innerText = books[bookIndex].name;
 });
+
+function mouseOver() {
+  document.getElementById('mouse').style.color = 'red';
+}
+
+function mouseOut() {
+  document.getElementById('mouse').style.color = 'black';
+}
+
+function loop() {
+  img.src = books[bookIndex].image;
+}
